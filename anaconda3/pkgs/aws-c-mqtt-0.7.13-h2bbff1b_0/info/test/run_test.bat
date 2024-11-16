f25d16f5659772c9f1c32a131f2134d25668db8f
Stefanija Lazareva <96191244+stefanijalazareva@users.noreply.github.com>
2024-11-16 11:24:21 +0100

    Delete anaconda3 directory

--- anaconda3/pkgs/aws-c-mqtt-0.7.13-h2bbff1b_0/info/test/run_test.bat
+++
@@ -1,9 +0,0 @@
-
-
-
-
-if not exist %LIBRARY_INC%\\aws\\mqtt\\mqtt.h exit 1
-IF %ERRORLEVEL% NEQ 0 exit /B 1
-if not exist %PREFIX%\\Library\\bin\\aws-c-mqtt.dll exit 1
-IF %ERRORLEVEL% NEQ 0 exit /B 1
-exit /B 0
