4735f789d8e6e86b2b5b68295c85d698363b5669
Stefanija Lazareva <96191244+stefanijalazareva@users.noreply.github.com>
2024-11-25 23:24:20 +0100

    Delete WP-2024-25/lab directory

--- WP-2024-25/lab/src/main/java/mk/finki/ukim/mk/lab/service/ArtistService.java
+++
@@ -1,9 +0,0 @@
-package mk.finki.ukim.mk.lab.service;
-
-import mk.finki.ukim.mk.lab.model.Artist;
-import java.util.List;
-
-public interface ArtistService {
-    List<Artist> listArtists();
-    Artist findById(Long id);
-}
